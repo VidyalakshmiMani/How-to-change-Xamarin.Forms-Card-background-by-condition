4eaccd51b2b34ea83c118ed671be12aa74cf7c3e
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-06-29 12:12:15 +0530

    Sample included

---
+++ Card_BackgroundColor/Card_BackgroundColor/Card_BackgroundColor/ContentLayout.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Card_BackgroundColor
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContentLayout : Grid
+    {
+        public ContentLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Card_BackgroundColor/Card_BackgroundColor/Card_BackgroundColor/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace Card_BackgroundColor
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class CustomDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ReportTemplate { get; set; }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (Convert.ToDouble(((Result)item).Marks) <= 40)
+            {
+                ((Result)item).BackgroundColor = Color.Red;
+                return ReportTemplate;
+            }
+            else
+            {
+                ((Result)item).BackgroundColor = Color.LightGreen;
+                return ReportTemplate;
+            }
+        }
+    }
+   
+    public class TestViewModel
+    {
+        public ObservableCollection<Result> Data { get; set; }
+
+        public TestViewModel()
+        {
+            Data = new ObservableCollection<Result>();
+            Data.Add(new Result { Name = "Jessica", Subject = "Math", Marks = 34 });
+            Data.Add(new Result { Name = "Alice", Subject = "Math", Marks = 75 });
+            Data.Add(new Result { Name = "Rebecca", Subject = "Math", Marks = 20 });
+            Data.Add(new Result { Name = "Alexander", Subject = "Math", Marks = 90 });
+        }
+    }
+
+    public class Result
+    {
+        public string Name { get; set; }
+       
+        public string Subject { get; set; }
+
+        public int Marks { get; set; }
+
+        public Color BackgroundColor { get; set; }
+    }
+}
